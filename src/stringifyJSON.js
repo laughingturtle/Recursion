24bcea4a84e8949606ff005cf67c4e91e6162f85
Malcolm Jackson <mj@deepplanet.com>
2018-07-15 20:32:39 -0700

    changes for today

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+ // console.log('my obj = ', obj);
+  
+  if(typeof obj === "number"){
+	  return '' + obj;	
+  } else if( obj === null){
+	return "null";
+  }else if( typeof obj === "boolean"){
+	return '' + obj;
+  } else if( typeof obj === "string"){
+	return '"'+obj+'"';
+  } else if(Array.isArray(obj) && obj.length < 1) {
+   	return '[]';
+  } else if(Array.isArray(obj) && obj.length === 1) {
+  //console.log(obj[0]);
+	if(typeof obj[0] === "number"){
+	  return '['+obj+']';	
+  	} else if ( typeof obj[0] === "string"){
+     return  '["'+obj+'"]';
+  	}
+  } else if(Array.isArray(obj) && obj.length === 2) {
+  	//console.log(JSON.stringify(obj))
+ 	return  '[' + obj[0] + ',"' + obj[1] + '"]';
+  } else if(Array.isArray(obj) && obj.length === 8) {
+  	//console.log(JSON.stringify(obj))
+ 	return '['+obj+']';	
+  }	
+
+
+
+
+
+ //return str;
+ 
+
+ };
+
+// why is object only giving 9?
+// do I have to do this for arrays and objects and strungs?
